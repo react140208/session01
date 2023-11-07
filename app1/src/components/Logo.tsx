921f2ccad04847c4648b0031c5b921d137218fc6
Pasha <pashaie@gmail.com>
2023-11-07 21:02:55 +0330

    logo destructure

--- app1/src/components/Logo.tsx
+++ app1/src/components/Logo.tsx
@@ -5,13 +5,19 @@ interface LogoProps {
   logo: string;
   rank?: number;
 }
-export default function Logo(props: LogoProps) {
-  console.log(props);
+// export default function Logo(props: LogoProps) {
+//destructure
+export default function Logo({ msg, logo }: LogoProps) {
+  // const msg = props.msg;
+  // const logo = props.logo;
+  // console.log(props);
   return (
     <div>
-      <img src={props.logo} alt="React" className="logo react" />
+      {/* <img src={props.logo} alt="React" className="logo react" /> */}
+      <img src={logo} alt="React" className="logo react" />
       {/* <h1>Salam Donya</h1> */}
-      <h1>{props.msg}</h1>
+      {/* <h1>{props.msg}</h1> */}
+      <h1>{msg}</h1>
     </div>
   );
 }
