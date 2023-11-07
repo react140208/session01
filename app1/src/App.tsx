c64d3f4bc09a1e3bfdf9fb2008647dce37198863
Pasha <pashaie@gmail.com>
2023-11-07 18:54:23 +0330

    jsx

--- app1/src/App.tsx
+++ app1/src/App.tsx
@@ -1,9 +1,26 @@
 import "./App.css";
 
+/*  JSX
+      js, ts -> html
+      single root
+        <> = fragment
+      elements -> close
+        <input type="text"></input>
+        <input type="text" />
+      class -> className 
+        <div id="root" class="xyz"></div> -> <div id="root" className="xyz"></div>
+      all properties use camelCase
+        styles: font-size -> fontSize
+        events: onclick -> onClick
+*/
+
 function App() {
+  let a = 10;
   return (
     <>
       <h1>Salam Donya</h1>
+      <h1>Salam Donya {a}</h1>
+      <input type="text" className={"xyz"} />
     </>
   );
 }
